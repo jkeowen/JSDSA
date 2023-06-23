5d260478c67b0bee2e86d3ead7e542c097c911b6
Justin Keowen <jkeowen@gmail.com>
2023-06-23 09:34:59 -0500

    second problem

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 "use strict";
 //Are Similar
+// const areSimilar = (firstNumberArray: number[], secondNumeberArray: number[]): boolean =>  {
 Object.defineProperty(exports, "__esModule", { value: true });
-var areSimilar = function (firstNumberArray, secondNumeberArray) {
-    var leftPointer = 0;
-    var rightPointer = 1;
-    var swapCount = 0;
-    while (firstNumberArray[rightPointer]) {
-        if (firstNumberArray[leftPointer] !== secondNumeberArray[leftPointer] && (firstNumberArray[leftPointer] === secondNumeberArray[rightPointer] && secondNumeberArray[leftPointer] === firstNumberArray[rightPointer])) {
-            var temp = firstNumberArray[1];
-            firstNumberArray[1] = firstNumberArray[0];
-            firstNumberArray[0] = temp;
-            swapCount++;
-        }
-        else if (firstNumberArray[leftPointer] !== secondNumeberArray[leftPointer] || firstNumberArray[rightPointer] !== secondNumeberArray[rightPointer]) {
-            return false;
-        }
-        if (swapCount > 1)
-            return false;
-        leftPointer++;
-        rightPointer++;
-    }
-    return true;
-};
-console.log(areSimilar([1, 2, 1, 2], [2, 2, 1, 1]));
+//   let leftPointer: number = 0;
+//   let rightPointer: number = 1;
+//   let swapCount: number = 0;
+//   while(firstNumberArray[rightPointer]){
+//     if(firstNumberArray[leftPointer] !== secondNumeberArray[leftPointer] && (firstNumberArray[leftPointer] === secondNumeberArray[rightPointer] && secondNumeberArray[leftPointer] === firstNumberArray[rightPointer])) {
+//       let temp: number = firstNumberArray[1];
+//       firstNumberArray[1] = firstNumberArray[0];
+//       firstNumberArray[0] = temp;
+//       swapCount++;
+//     }
+//     else if(firstNumberArray[leftPointer] !== secondNumeberArray[leftPointer] || firstNumberArray[rightPointer] !== secondNumeberArray[rightPointer]){
+//       return false;
+//     } 
+//     if(swapCount > 1) return false
+//     leftPointer++;
+//     rightPointer++;
+//   }
+//   return true;
+// } 
+// console.log(areSimilar([1,2,1,2], [2,2,1,1]))
+//Sum Strings as Numbers
+var sumStrings = function (firstNumber, secondNumber) { return String(BigInt(firstNumber) + BigInt(secondNumber)); };
+console.log(sumStrings('712569312664357328695151392', '8100824045303269669937'));
--- index.ts
+++ index.ts
@@ -1,27 +1,32 @@
 //Are Similar
-
-const areSimilar = (firstNumberArray: number[], secondNumeberArray: number[]): boolean =>  {
+// const areSimilar = (firstNumberArray: number[], secondNumeberArray: number[]): boolean =>  {
   
-  let leftPointer: number = 0;
-  let rightPointer: number = 1;
-  let swapCount: number = 0;
-  while(firstNumberArray[rightPointer]){
-    if(firstNumberArray[leftPointer] !== secondNumeberArray[leftPointer] && (firstNumberArray[leftPointer] === secondNumeberArray[rightPointer] && secondNumeberArray[leftPointer] === firstNumberArray[rightPointer])) {
-      let temp: number = firstNumberArray[1];
-      firstNumberArray[1] = firstNumberArray[0];
-      firstNumberArray[0] = temp;
-      swapCount++;
-    }
-    else if(firstNumberArray[leftPointer] !== secondNumeberArray[leftPointer] || firstNumberArray[rightPointer] !== secondNumeberArray[rightPointer]){
-      return false;
-    } 
-    if(swapCount > 1) return false
-    leftPointer++;
-    rightPointer++;
-  }
+//   let leftPointer: number = 0;
+//   let rightPointer: number = 1;
+//   let swapCount: number = 0;
+//   while(firstNumberArray[rightPointer]){
+//     if(firstNumberArray[leftPointer] !== secondNumeberArray[leftPointer] && (firstNumberArray[leftPointer] === secondNumeberArray[rightPointer] && secondNumeberArray[leftPointer] === firstNumberArray[rightPointer])) {
+//       let temp: number = firstNumberArray[1];
+//       firstNumberArray[1] = firstNumberArray[0];
+//       firstNumberArray[0] = temp;
+//       swapCount++;
+//     }
+//     else if(firstNumberArray[leftPointer] !== secondNumeberArray[leftPointer] || firstNumberArray[rightPointer] !== secondNumeberArray[rightPointer]){
+//       return false;
+//     } 
+//     if(swapCount > 1) return false
+//     leftPointer++;
+//     rightPointer++;
+//   }
   
-  return true;
-} 
+//   return true;
+// } 
+// console.log(areSimilar([1,2,1,2], [2,2,1,1]))
 
-console.log(areSimilar([1,2,1,2], [2,2,1,1]))
+//Sum Strings as Numbers
+
+const sumStrings = (firstNumber: string, secondNumber: string): string => String(BigInt(firstNumber) + BigInt(secondNumber))
+
+console.log(sumStrings('712569312664357328695151392', '8100824045303269669937'));
+
 export{}
